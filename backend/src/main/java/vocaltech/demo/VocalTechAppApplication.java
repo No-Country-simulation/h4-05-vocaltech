7c9663f563855d15d6e6e2cf1a00d14bc571e13b
MartinKun <martinkunbrc@hotmail.com>
2025-01-08 23:27:23 -0300

    feat: init backend project

---
+++ backend/src/main/java/vocaltech/demo/VocalTechAppApplication.java
@@ -0,0 +1,13 @@
+package vocaltech.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocalTechAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocalTechAppApplication.class, args);
+	}
+
+}
