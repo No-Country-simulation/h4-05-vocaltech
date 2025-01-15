32d4dd23b5aa4cb79b7bd9358fb18a8b86a348b4
Maria Alarcon <mariagaa993@gmail.com>
2025-01-14 21:08:37 -0300

    estructura básica de las vistas emprendedor/empresa

--- frontend/src/views/Company.js
+++ frontend/src/views/Company.js
@@ -1,3 +1,26 @@
-export const Company = () => {
-    return ("Company");
-}
+export const Company = () => (
+    <div className="container-fluid p-0">
+        <h1 className="text-center">Empresa</h1>
+        <section className="container pb-5">
+            <h3 className="pt-5 pb-3">Servicios</h3>
+            <ul>
+                <li>Workshops y coaching de comunicación y liderazgo para equipos de trabajo</li>
+                <li>Búsqueda y selección de talento a través de la observación de comportamiento 
+                    mediante simulaciones laborales</li>
+            </ul>
+        </section>
+        <section className="bg-warning p-5 text-center">
+            <button className="btn btn-primary">Diagnóstico</button>
+        </section>
+        <section className="container">
+            <h3 className="pt-5 pb-3">Testimonios</h3>
+            <div className="w-25 bg-secondary">
+                <p>Maria</p>
+                <p>Esto es el texto de un testimonio</p>
+            </div>
+        </section>  
+        <section className="container pb-5">
+            <h3 className="pt-5 pb-3">Contacto</h3>
+        </section>   
+    </div>
+);
--- frontend/src/views/Entrepreneur.js
+++ frontend/src/views/Entrepreneur.js
@@ -1,3 +1,25 @@
-export const Entrepreneur = () => {
-    return ("Entrepreneur");
-}
+export const Entrepreneur = () => (
+    <div className="container-fluid p-0">
+        <h1 className="text-center">Emprendedor</h1>
+        <section className="container pb-5">
+            <h3 className="pt-5 pb-3">Servicios</h3>
+            <ul>
+                <li>Coaching de comunicación y liderazgo</li>
+                <li>Desarrollo de MVPs de alta fidelidad en 5 semanas</li>
+            </ul>
+        </section>
+        <section className="bg-warning p-5 text-center">
+            <button className="btn btn-primary">Diagnóstico</button>
+        </section>
+        <section className="container">
+            <h3 className="pt-5 pb-3">Testimonios</h3>
+            <div className="w-25 bg-secondary">
+                <p>Maria</p>
+                <p>Esto es el texto de un testimonio</p>
+            </div>
+        </section>  
+        <section className="container pb-5">
+            <h3 className="pt-5 pb-3">Contacto</h3>
+        </section>   
+    </div>
+);
