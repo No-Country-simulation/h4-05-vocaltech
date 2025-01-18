14b6a07d9dc0ea9ab9358c6108b6f85051f1ff66
Maria Alarcon <mariagaa993@gmail.com>
2025-01-17 23:18:13 -0300

    crear servicio para el audio

---
+++ frontend/src/services/audioRecorder.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+const audioFile = async (mediaBlobUrl) => {
+    try {
+        const response = await axios.get(mediaBlobUrl, { responseType: 'blob' });
+        const blob = response.data; 
+          
+        const audioFile = new File([blob], 'audio.wav', { type: 'audio/wav' });
+
+        return audioFile;
+    } catch  {
+        throw new Error("Error al obtener el blob. Inténtalo de nuevo más tarde.");
+    }
+};
+
+const uploadToCloudinary = async (file) => {
+    const formData = new FormData();
+    formData.append('file', file);  
+    formData.append('upload_preset', 'vocaltech_preset'); 
+
+    try {
+        const response = await axios.post('https://api.cloudinary.com/v1_1/dc2j8zklr/upload', formData);
+        const url = response.data.secure_url; 
+
+        return url;
+    } catch {
+        throw new Error("Error al subir audio al servidor. Inténtalo de nuevo más tarde.");
+    }
+}
+
+export const audioRecorderService = {
+    audioFile,
+    uploadToCloudinary,
+} 
