1796bd1145988eb214ebd30e9fe2d44fa6f587e1
Maria Alarcon <mariagaa993@gmail.com>
2025-01-20 22:09:46 -0300

    componente footer con clases de bootstrap e iconos de fontawesome

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,40 +1,41 @@
-import { CDBBox, CDBBtn, CDBIcon } from 'cdbreact';  
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; 
+import { socialLinks } from "../utils/socialLinks";
 
-export const FooterVocalTech = () => {  
-  return (  
-    <CDBBox className="shadow fixed-bottom" style={{ backgroundColor: '#21081a', color: 'white', padding: '20px' }}>  
-      <CDBBox  
-        display="flex"  
-        justifyContent="between"  
-        alignItems="center"  
-        className="mx-auto flex-wrap"  
-        style={{ width: '80%' }}  
-      >  
-        <CDBBox display="flex" alignItems="center">  
-          <a href="/" className="d-flex align-items-center p-0 text-light">  
-            {/* <img  
-              alt="logo"  
-              src=""   
-              width="30px"  
-            />   */}
-            <span className="ms-4 h5 mb-0 font-weight-bold">VocalTech</span>  
-          </a>  
-        </CDBBox>  
-        <CDBBox>  
-          <small className="ms-2">&copy; VocalTech, 2024. Derechos reservados.</small>  
-        </CDBBox>  
-        <CDBBox display="flex">  
-          <CDBBtn flat color="dark" className="p-2">  
-            <CDBIcon fab icon="facebook-f" />  
-          </CDBBtn>  
-          <CDBBtn flat color="dark" className="mx-3 p-2">  
-            <CDBIcon fab icon="twitter" />  
-          </CDBBtn>  
-          <CDBBtn flat color="dark" className="p-2">  
-            <CDBIcon fab icon="instagram" />  
-          </CDBBtn>  
-        </CDBBox>  
-      </CDBBox>  
-    </CDBBox>  
-  );  
+export const FooterVocalTech = () => {
+    return (
+        <footer className="bg-dark py-4">
+            <div className="container">
+                <div className="row d-flex align-items-center">
+                    <div className="col-md-4 pb-5 pb-md-0 text-center text-md-start">
+                        <Link className="navbar-brand text-white fs-2" to="/">
+                            VocalTech
+                        </Link>
+                    </div>
+                    <div className="col-md-4 d-none d-md-block text-center">
+                        <small className="text-white">&copy;2024 - VocalTech. Derechos reservados.</small>
+                    </div>
+                    <div className="col-md-4 d-flex justify-content-center justify-content-md-end pb-5 pb-md-0">
+                        {
+                            socialLinks.map((link, index) => (
+                                <a
+                                    key={index}
+                                    href={link.url}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="btn btn-light rounded-circle d-flex justify-content-center 
+                                    align-items-center mx-2 mx-md-0 ms-md-2"
+                                    style={{ width: '40px', height: '40px' }}>
+                                    <FontAwesomeIcon icon={link.icon} />
+                                </a>
+                            ))
+                        }
+                    </div>
+                    <div className="d-md-none text-center">
+                        <small className="text-white">&copy;2024 - VocalTech. Derechos reservados.</small>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
 };
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,48 +4,50 @@ import "../styles/navbar.css"
 
 export const NavbarVocalTech = () => {
     return (
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark sticky-top px-2 px-lg-5">
-            <Link className="navbar-brand text-white" to="/">
-                VocalTech
-            </Link>
-            <button
-                className="navbar-toggler text-white"
-                type="button"
-                data-bs-toggle="offcanvas"
-                data-bs-target="#offcanvasNavbar"
-                aria-controls="offcanvasNavbar">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div
-                className="offcanvas offcanvas-end bg-dark"
-                tabIndex="-1"
-                id="offcanvasNavbar"
-                aria-labelledby="offcanvasNavbarLabel">
-                <div className="offcanvas-header bg-dark">
-                    <h5 className="offcanvas-title text-white" id="offcanvasNavbarLabel">
-                        VocalTech
-                    </h5>
-                    <button
-                        type="button"
-                        className="btn-close btn-close-white"
-                        data-bs-dismiss="offcanvas"
-                        aria-label="Close">
-                    </button>
-                </div>
-                <div className="offcanvas-body bg-dark">
-                    <ul className="navbar-nav justify-content-end flex-grow-1">
-                        {
-                            navLinks.map((link, index) => (
-                                <li className="nav-item" key={index}>
-                                    <Link className="nav-link text-white ms-4" to={link.to}>
-                                        {link.label}
-                                    </Link>
-                                </li>
-                            ))
-                        }
-                    </ul>
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark sticky-top">
+            <div className="container">
+                <Link className="navbar-brand text-white" to="/">
+                    VocalTech
+                </Link>
+                <button
+                    className="navbar-toggler text-white"
+                    type="button"
+                    data-bs-toggle="offcanvas"
+                    data-bs-target="#offcanvasNavbar"
+                    aria-controls="offcanvasNavbar">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div
+                    className="offcanvas offcanvas-end bg-dark"
+                    tabIndex="-1"
+                    id="offcanvasNavbar"
+                    aria-labelledby="offcanvasNavbarLabel">
+                    <div className="offcanvas-header bg-dark">
+                        <h5 className="offcanvas-title text-white" id="offcanvasNavbarLabel">
+                            VocalTech
+                        </h5>
+                        <button
+                            type="button"
+                            className="btn-close btn-close-white"
+                            data-bs-dismiss="offcanvas"
+                            aria-label="Close">
+                        </button>
+                    </div>
+                    <div className="offcanvas-body bg-dark">
+                        <ul className="navbar-nav justify-content-end flex-grow-1">
+                            {
+                                navLinks.map((link, index) => (
+                                    <li className="nav-item" key={index}>
+                                        <Link className="nav-link text-white ms-4" to={link.to}>
+                                            {link.label}
+                                        </Link>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </nav>
+        </nav>    
     );
 };
