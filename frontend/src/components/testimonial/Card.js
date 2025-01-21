964bb21a76d80670dbf25579862c592294403687
Maria Alarcon <mariagaa993@gmail.com>
2025-01-21 12:32:59 -0300

    crear componente card para mostrar los testimonios

--- frontend/src/components/testimonial/Card.js
+++ frontend/src/components/testimonial/Card.js
@@ -2,35 +2,30 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 import { faStar as regularStar } from '@fortawesome/free-regular-svg-icons';
 
-export const Card = () => {
+export const Card = ({ name, role, description, imgUrl, stars  }) => {
     return (
-        <div className="card py-3 px-1 col-md-4 col-lg-3">
+        <div className="card py-3 px-1" style={{ minHeight: '400px' }}>
             <div className="card-header border-0 bg-transparent">
-                    <FontAwesomeIcon icon={faStar} className="text-warning fs-5" />
-                    <FontAwesomeIcon icon={faStar} className="text-warning fs-5" />
-                    <FontAwesomeIcon icon={faStar} className="text-warning fs-5" />
-                    <FontAwesomeIcon icon={faStar} className="text-warning fs-5" />
-                    <FontAwesomeIcon icon={regularStar} className="text-warning fs-5" />
-             
+                {[...Array(stars)].map((_, index) => (
+                    <FontAwesomeIcon key={index} icon={faStar} className="text-warning fs-6" />
+                ))}
+                {[...Array(5 - stars)].map((_, index) => (
+                    <FontAwesomeIcon key={index + stars} icon={regularStar} className="text-warning fs-6" />
+                ))}
             </div>
             <div className="card-body">
-                <p className="card-text">
-                    La capacitación en técnicas de pitch ha transformado nuestra
-                    forma de presentar proyectos. Ahora tenemos las herramientas
-                    necesarias para comunicar nuestras ideas de manera clara y
-                    efectiva.
-                </p>
+                <p className="card-text">{description}</p>
             </div>
             <div className="card-footer d-flex border-0 bg-transparent">
                 <img
-                    src="https://picsum.photos/200?random=3"
-                    alt="Juan Pérez"
+                    src={imgUrl}
+                    alt={name}
                     className="object-fit-contain rounded-circle me-4"
-                    style={{ width: "80px", height: "80px" }}
+                    style={{ width: "70px", height: "70px" }}
                 />
                 <div className="align-self-center">
-                    <p className="card-title">Juan Pérez</p>
-                    <p className="card-subtitle fw-bold">CEO de XYZ</p>
+                    <p className="card-title">{name}</p>
+                    <p className="card-subtitle fw-bold">{role}</p>
                 </div>
             </div>
         </div>
