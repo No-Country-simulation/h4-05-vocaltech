2b622764ed9c5d0863934d9f1aa0ed7242bfc1ea
Juanmita <juanmapimientae@gmail.com>
2025-01-22 14:55:59 -0500

    Creation of Appointment entity with repository, service and controller

---
+++ backend/src/main/java/vocaltech/demo/persistence/repository/AppointmentRepository.java
@@ -0,0 +1,9 @@
+package com.example.appointmentsystem.repository;
+
+import com.example.appointmentsystem.entity.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+}
