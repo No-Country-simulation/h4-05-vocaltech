710272da3726015c9b6e93d8caa3089ee6520fa3
Maria Alarcon <mariagaa993@gmail.com>
2025-01-22 21:44:11 -0300

    agregar componente tabla en la vista de leads

--- frontend/src/views/admin/Leads.js
+++ frontend/src/views/admin/Leads.js
@@ -1,7 +1,35 @@
+import { useState, useEffect } from "react";
+import { columnsTable } from "../../utils/columnsTable";
+import { Table } from "../../components/admin/Table";
+import { prueba } from "../../utils/infoPrueba"; 
+import { useCompanySelect } from "../../contexts/CompanySelected";
+
 export const Leads = () => {
+    const [leadsData, setLeadsData] = useState([]);
+    const { selectedValue } = useCompanySelect();
+    
+    useEffect(() => {
+        const processedLeads = prueba.leads.map(item => ({
+            ...item,
+            diagnostic: item.diagnostic ? 'Si' : 'No', 
+            plan: item.plan ? 'Si' : 'No',
+        }));
+
+        setLeadsData(processedLeads);
+    }, []);
+
     return (
-        <>
-        <h1>Leads</h1>
-        </>
+        <section>
+            <h2 className="pb-5">Leads</h2>
+                {
+                    selectedValue === "Vos y tu Voz" ? (
+                        'Leads Vos y tu voz'
+                    ) : selectedValue === "No Country" ? (
+                        'Leads No Country'
+                    ) : (
+                        <Table columns={columnsTable.leads} data={leadsData} />
+                    )
+                }
+        </section>
     );
 };
