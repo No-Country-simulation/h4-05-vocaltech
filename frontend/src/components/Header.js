b0fdd21b842d060274d61a17ec637814d867265d
santgeek <santiago.mordz@gmail.com>
2025-01-23 13:37:46 -0500

    styles(code update)

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,7 +3,17 @@ export const Header = () => {
     <section className="container-fluid bg-info">
       <div className="container">
         <div className="row">
-          <div className="col-md-6 d-none d-md-block" style={{backgroundImage: "url(./images/banner-home.png)", backgroundSize: "contain", backgroundRepeat: "no-repeat", backgroundPosition: "center", height: "600px"}}></div> {/*  se muestra a partir de 768px */}
+          <div
+            className="col-md-6 d-none d-md-block"
+            style={{
+              backgroundImage: "url(./images/banner-home.png)",
+              backgroundSize: "contain",
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "center",
+              height: "600px",
+            }}
+          ></div>{" "}
+          {/*  solo se muestra a partir de 768px */}
           <div className="col-md-6 pb-5 align-self-center">
             <h2 className="display-5 text-center fw-bolder mt-5 mb-3">
               Trabajamos con tu voz para impulsar tus ideas
@@ -22,12 +32,19 @@ export const Header = () => {
               </button>
             </div>
           </div>
-          <div className="col-md-6 d-md-none" style={{backgroundImage: "url(./images/banner-home.png)", width: "100%", height: "380px", backgroundPosition: "center", backgroundSize: "cover"}}></div>
-          {/* <div className="col-md-6 d-md-none" style={{backgroundImage: "url(./images/banner-home.png)", height: "200px"}}> {/*  solo se ve en mobile */}
-           
+          <div
+            className="col-md-6 d-md-none"
+            style={{
+              backgroundImage: "url(./images/banner-home.png)",
+              width: "100%",
+              height: "380px",
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+            }}
+          ></div>{" "}
+          {/*  solo se ve en mobile */}
         </div>
-        </div>
-      
+      </div>
     </section>
   );
 };
