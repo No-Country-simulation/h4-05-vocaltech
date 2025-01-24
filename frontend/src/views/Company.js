bf8f138b8b13c5dffa97eb8f1fdfcd0b24f5a6ad
Maria Alarcon <mariagaa993@gmail.com>
2025-01-24 09:16:36 -0300

    agregar seccion de servicios emprendedor/empresa

--- frontend/src/views/Company.js
+++ frontend/src/views/Company.js
@@ -1,16 +1,10 @@
+import { Services } from "../components/company/Services";
 import { Testimonials } from "../components/testimonial/Testimonials";
 
 export const Company = () => (
     <section className="company">
         <h1 className="text-center">Empresa</h1>
-        <section className="container pb-5">
-            <h3 className="pt-5 pb-3">Servicios</h3>
-            <ul>
-                <li>Workshops y coaching de comunicación y liderazgo para equipos de trabajo</li>
-                <li>Búsqueda y selección de talento a través de la observación de comportamiento 
-                    mediante simulaciones laborales</li>
-            </ul>
-        </section>
+        <Services />
         <section className="bg-warning p-5 text-center">
             <button className="btn btn-primary">Diagnóstico</button>
         </section>
--- frontend/src/views/Entrepreneur.js
+++ frontend/src/views/Entrepreneur.js
@@ -1,16 +1,11 @@
+import { Services } from "../components/entrepreneur/Services";
 import { Testimonials } from "../components/testimonial/Testimonials";
 
 export const Entrepreneur = () => (
     <section className="entrepreneur">
         <h1 className="text-center">Emprendedor</h1>
-        <section className="container pb-5">
-            <h3 className="pt-5 pb-3">Servicios</h3>
-            <ul>
-                <li>Coaching de comunicación y liderazgo</li>
-                <li>Desarrollo de MVPs de alta fidelidad en 5 semanas</li>
-            </ul>
-        </section>
-        <section className="bg-warning p-5 text-center">
+        <Services />
+       <section className="bg-warning p-5 text-center">
             <button className="btn btn-primary">Diagnóstico</button>
         </section>
         <Testimonials />
