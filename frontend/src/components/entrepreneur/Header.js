d73fd1c53722a2caa162bf5463f8e72adf569291
Maria Alarcon <mariagaa993@gmail.com>
2025-01-24 10:58:41 -0300

    agregar header en la vista del emprendedor

---
+++ frontend/src/components/entrepreneur/Header.js
@@ -0,0 +1,30 @@
+import "../../styles/header.css";
+
+export const Header = () => {
+    return (
+        <section className="container py-5">
+            <div className="row align-items-center">
+                <div className="col-md-6 text-center text-md-start mb-5 mb-md-0">
+                    <h1 className="display-5 fw-bolder">Impulsa tu emprendimiento</h1>
+                    <p className="lead py-4">Lleva tus ideas al siguiente nivel con nuestros 
+                        servicios y descubre herramientas y estrategias personalizadas para tu negocio
+                    </p>
+                    <button type="button"
+                        className="btn btn-dark rounded-pill btn-width-services">
+                        Solicitar Diagnóstico
+                    </button>
+                </div>
+                <div className="col-md-6 text-center">
+                    <div className="img-wrapper position-relative">
+                        <img
+                            src="./images/emprendedor.jpg"
+                            className="img-fluid rounded-circle shadow-lg img-entrepreneur"
+                            alt="Emprendedor"
+                        />
+                        <div className="circle-bg position-absolute"></div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
--- frontend/src/views/Entrepreneur.js
+++ frontend/src/views/Entrepreneur.js
@@ -1,9 +1,12 @@
+import { Header } from "../components/entrepreneur/Header";
 import { Services } from "../components/entrepreneur/Services";
 import { Testimonials } from "../components/testimonial/Testimonials";
 
 export const Entrepreneur = () => (
     <section className="entrepreneur">
-        <h1 className="text-center">Emprendedor</h1>
+        <section className="entrepreneur-header px-0 pt-0 pb-xl-0">
+            <Header />
+        </section>
         <Services />
        <section className="bg-warning p-5 text-center">
             <button className="btn btn-primary">Diagnóstico</button>
