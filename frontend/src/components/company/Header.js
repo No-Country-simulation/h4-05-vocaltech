4d582cf9df69746acef1bf152370a6ca22186f90
Maria Alarcon <mariagaa993@gmail.com>
2025-01-24 12:49:17 -0300

    agregar header de la vista empresa

---
+++ frontend/src/components/company/Header.js
@@ -0,0 +1,42 @@
+import "../../styles/header.css";
+
+export const Header = () => {
+    return (
+        <section className="container py-5">
+            <div className="row align-items-center">
+                <div className="col-md-6 text-center d-none d-md-block">
+                    <div className="img-wrapper position-relative">
+                        <div className="circle-bg-left position-absolute"></div>
+                        <img
+                            src="./images/empresa.jpg"
+                            className="img-fluid rounded-circle shadow-lg img-company"
+                            alt="Empresa"
+                        />
+                        <div className="circle-bg-right position-absolute"></div>
+                    </div>
+                </div>
+                <div className="col-md-6 text-center text-md-start mb-5 mb-md-0">
+                    <h1 className="display-5 fw-bolder">Potencia tu empresa</h1>
+                    <p className="lead py-4">Ofrecemos soluciones innovadoras que impulsan el crecimiento 
+                        y éxito de tu organización.
+                    </p>
+                    <button type="button"
+                        className="btn btn-dark rounded-pill btn-width-services">
+                        Solicitar Diagnóstico
+                    </button>
+                </div>
+                <div className="col-md-6 text-center d-md-none">
+                    <div className="img-wrapper position-relative">
+                        <div className="circle-bg-left position-absolute"></div>
+                        <img
+                            src="./images/empresa.jpg"
+                            className="img-fluid rounded-circle shadow-lg img-company"
+                            alt="Empresa"
+                        />
+                        <div className="circle-bg position-absolute"></div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
--- frontend/src/views/Company.js
+++ frontend/src/views/Company.js
@@ -1,9 +1,12 @@
+import { Header } from "../components/company/Header";
 import { Services } from "../components/company/Services";
 import { Testimonials } from "../components/testimonial/Testimonials";
 
 export const Company = () => (
     <section className="company">
-        <h1 className="text-center">Empresa</h1>
+        <section className="company-header px-0 pt-0 pb-xl-0">
+            <Header />
+        </section>
         <Services />
         <section className="bg-warning p-5 text-center">
             <button className="btn btn-primary">Diagnóstico</button>
