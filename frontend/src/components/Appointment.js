4b96abdd3ccbea61c892aebee6d695feea035975
Maria Alarcon <mariagaa993@gmail.com>
2025-01-26 12:42:21 -0300

    personalizar el calendario

--- frontend/src/components/Appointment.js
+++ frontend/src/components/Appointment.js
@@ -3,7 +3,7 @@ import { InlineWidget } from 'react-calendly';
 export const Appointment = () => {
     return (
         <div className="container pt-5 pt-md-0">
-            <InlineWidget url="https://calendly.com/mariagaa993/30min" /> 
+            <InlineWidget url="https://calendly.com/mariagaa993/30min?background_color=F0F0F0&text_color=333333&primary_color=FF5733" /> 
         </div>
     );
 };
