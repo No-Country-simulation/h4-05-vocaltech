c19c7e4933dfb203cb7f742186f29d6cb934de67
Maria Alarcon <mariagaa993@gmail.com>
2025-01-26 12:35:01 -0300

    agregar estilos vista del calendario

--- frontend/src/components/Appointment.js
+++ frontend/src/components/Appointment.js
@@ -2,7 +2,7 @@ import { InlineWidget } from 'react-calendly';
 
 export const Appointment = () => {
     return (
-        <div className="container">
+        <div className="container pt-5 pt-md-0">
             <InlineWidget url="https://calendly.com/mariagaa993/30min" /> 
         </div>
     );
--- frontend/src/components/diagnostic/Header.js
+++ frontend/src/components/diagnostic/Header.js
@@ -1,14 +1,16 @@
 export const Header = () => {
     return (
         <section className="container py-5">
-            <div className="row align-items-center">
+            <div className="row">
                 <div className="text-center">
                     <h1 className="display-5 fw-bolder" 
                         style={{background: "linear-gradient(135deg, #6a11cb, #2575fc)", 
                         WebkitBackgroundClip: "text",  
-                        WebkitTextFillColor: "transparent"}}>¡Haz tu Diagnóstico!</h1>
+                        WebkitTextFillColor: "transparent"}}>¡Haz tu Diagnóstico!
+                    </h1>
                     <p className="lead pt-2">Responde algunas preguntas y recibe un análisis personalizado 
-                        de tus necesidades.</p>
+                        de tus necesidades.
+                    </p>
                 </div>
             </div>
         </section>
