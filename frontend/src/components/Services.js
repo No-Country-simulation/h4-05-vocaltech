c0b0823b5549ae152764086a096bfd36fd118ff3
Maria Alarcon <mariagaa993@gmail.com>
2025-01-26 21:55:47 -0300

    agregar sección para mostrar beneficios. Cambiar textos del servicio

--- frontend/src/components/Services.js
+++ frontend/src/components/Services.js
@@ -1,20 +1,72 @@
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHouseLaptop } from "@fortawesome/free-solid-svg-icons";
-import { faBriefcase } from "@fortawesome/free-solid-svg-icons";
+import { faLightbulb } from "@fortawesome/free-solid-svg-icons";
+import { faBuilding } from "@fortawesome/free-solid-svg-icons";
 
 export const Services = () => {
     return (
         <section className="container py-5">
-            <h2 className="display-7 fw-bolder text-center pb-5">Nuestros servicios</h2>
-            <div className="row mb-4">
-                <div className="col-xl-6 pe-lg-5 pe-xl-0">
-                    <h4 className="fw-bolder display-8 mb-3">Empresas</h4>
-                    <p className="mb-3">
-                        Fortalece tus equipos, lidera con confianza y transforma tu cultura
-                        empresarial. Descubre cómo nuestros workshops y herramientas
-                        innovadoras pueden llevar a tu empresa al siguiente nivel.
-                    </p>
-                    <p className="fw-bold mb-1">Beneficios:</p>
+            <h2 className="display-7 fw-bolder text-center pb-5">Nuestros Servicios</h2>
+            <div className="row">
+                <div className="col-md-6 pe-md-4">
+                    <div className="card mb-4 border-0">
+                        <img src="./images/emprendedores.jpg" className="rounded d-md-none" 
+                            alt="Emprendedores" />
+                        <div className="card-body px-0 pb-0 pt-md-0 pb-md-5">
+                            <h4 className="fw-bolder display-8">Para Emprendedores</h4>
+                            <p className="lead mb-3">Lleva tu idea al siguiente nivel</p>
+                            <p className="mb-3">
+                                Si eres emprendedor, te ayudamos a transformar tu visión en realidad. 
+                                Con servicios de coaching en liderazgo y desarrollo de productos (MVPs), 
+                                obtendrás las claves necesarias para dar el salto que tu proyecto necesita.
+                            </p>
+                            <p className="pb-4 fw-bold">¿Deseas saber cómo ayudamos a los emprendedores 
+                                como tú?
+                            </p>
+                            <Link to="/emprendedor">
+                                <button
+                                    type="button"
+                                    className="btn btn-outline-dark rounded-pill btn-width-services">
+                                    <FontAwesomeIcon className="me-1" icon={faLightbulb} /> Descubrir más
+                                </button>
+                            </Link>
+                        </div>
+                        <img src="./images/empresas.jpg" className="rounded d-none d-md-block" alt="Empresas" />
+                    </div>
+                </div>
+                <div className="col-md-6 pt-4 pt-md-0 ps-md-4">
+                    <div className="card mb-4 border-0">
+                        <img src="./images/empresas.jpg" className="rounded d-md-none" alt="Empresas" />
+                        <img src="./images/emprendedores.jpg" className="rounded d-none d-md-block" 
+                            alt="Emprendedores" />
+                        <div className="card-body px-0 pb-0 pt-md-4">
+                            <h4 className="fw-bolder display-8 mb-1">Para Empresas</h4>
+                            <p className="lead mb-3">Potencia tu equipo con soluciones personalizadas</p>
+                            <p className="mb-3">
+                                Descubre servicios diseñados para impulsar la comunicación, liderazgo y 
+                                gestión del talento en tu empresa. Desde workshops especializados hasta 
+                                asesoría estratégica, ofrecemos herramientas prácticas para que tu 
+                                organización crezca con éxito.
+                            </p>
+                            <p className="pb-4 fw-bold">¿Quieres conocer cómo potenciamos a las empresas?</p>
+                            <Link to="/empresa">
+                                <button
+                                    type="button"
+                                    className="btn btn-outline-dark rounded-pill btn-width-services">
+                                        <FontAwesomeIcon className="me-1" icon={faBuilding} /> Descubrir más
+                                </button>
+                            </Link>    
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+
+/* 
+<p className="fw-bold mb-1">Beneficios:</p>
                     <ul className="list-group list-group-flush mb-4">
                         <li className="list-group-item list-group-item-action">
                             Workshops en liderazgo y comunicación para equipos.
@@ -30,35 +82,13 @@ export const Services = () => {
                         </li>
                     </ul>
                     <p className="pb-4"><strong>Descubre cómo potenciamos a las empresas</strong></p>
-                    <div className="text-center pb-5 mb-md-3 mb-xl-4">
-                        <button
-                            type="button"
-                            className="btn btn-outline-dark rounded-pill btn-width">
-                            <FontAwesomeIcon className="me-2" icon={faBriefcase} /> Descubrir
-                        </button>
-                    </div>
-                </div>
-                <div className="col-md-6 pb-4">
-                    <img
-                        src="./images/empresas.jpg"
-                        alt="empresas"
-                        className="img-fluid rounded"
-                    />
-                </div>
-                <div className="col-md-6">
-                    <img
-                        src="./images/emprendedores.jpg"
-                        alt="empresas"
-                        className="img-fluid rounded"
-                    />
-                </div>
-                <div className="col-xl-6  pe-lg-5 pe-xl-0">
-                    <h4 className="fw-bolder display-8 pt-4 mb-3">Emprendedores</h4>
-                    <p className="mb-3">
-                        Transforma tu visión en un proyecto exitoso con nuestro
-                        acompañamiento. Desde el desarrollo de tu MVP hasta mejorar tu
-                        pitch, estamos aquí para ayudarte.
-                    </p>
+
+
+
+
+
+
+
                     <p className="fw-bold mb-1">Beneficios:</p>
                     <ul className="list-group list-group-flush mb-4">
                         <li className="list-group-item list-group-item-action">
@@ -74,16 +104,5 @@ export const Services = () => {
                             Estrategias de comunicación efectiva para ventas.{" "}
                         </li>
                     </ul>
-                    <p className="pb-4"><strong>Descubre cómo ayudadamos a los emprendedores</strong></p>
-                    <div className="text-center">
-                        <button
-                            type="button"
-                            className="btn btn-outline-dark rounded-pill btn-width">
-                            <FontAwesomeIcon className="me-2" icon={faHouseLaptop} /> Descubrir
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
-};
+
+*/
--- frontend/src/views/home.js
+++ frontend/src/views/home.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Header } from "../components/Header";
 import { Partners } from "../components/Partners";
 import { Services } from "../components/Services";
+import { Benefits } from "../components/Benefits";
 import { Testimonials } from "../components/testimonial/Testimonials";
 import { Diagnostic } from "../components/Diagnostic";
 
@@ -11,6 +12,7 @@ export const Home = () => {
       <Header />
       <Partners />
       <Services />
+      <Benefits />
       <Testimonials />
       <Diagnostic />   
     </>
