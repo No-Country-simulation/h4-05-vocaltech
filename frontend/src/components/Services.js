ca49446cf298706bf04bdc55dc2fdc2842a76efa
santgeek <santiago.mordz@gmail.com>
2025-01-27 17:15:09 -0500

    styles(colors): adjusting final colors for the project

--- frontend/src/components/Services.js
+++ frontend/src/components/Services.js
@@ -9,11 +9,11 @@ export const Services = () => {
             <h2 className="display-7 fw-bolder text-center pb-5">Nuestros Servicios</h2>
             <div className="row">
                 <div className="col-md-6 pe-md-4">
-                    <div className="card mb-4 border-0">
+                    <div className="card mb-4 border-0 bg-transparent">
                         <img src="./images/emprendedores.jpg" className="rounded d-md-none" 
                             alt="Emprendedores" />
                         <div className="card-body px-0 pb-0 pt-md-0 pb-md-5">
-                            <h4 className="fw-bolder display-8">Para Emprendedores</h4>
+                            <h4 className="fw-bolder display-8 mb-1 text-center">Para Emprendedores</h4>
                             <p className="lead mb-3">Lleva tu idea al siguiente nivel</p>
                             <p className="mb-3">
                                 Si eres emprendedor, te ayudamos a transformar tu visión en realidad. 
@@ -23,6 +23,7 @@ export const Services = () => {
                             <p className="pb-4 fw-bold">¿Deseas saber cómo ayudamos a los emprendedores 
                                 como tú?
                             </p>
+                            <div className="text-center">
                             <Link to="/emprendedor">
                                 <button
                                     type="button"
@@ -30,17 +31,18 @@ export const Services = () => {
                                     <FontAwesomeIcon className="me-1" icon={faLightbulb} /> Descubrir más
                                 </button>
                             </Link>
+                            </div>
                         </div>
                         <img src="./images/empresas.jpg" className="rounded d-none d-md-block" alt="Empresas" />
                     </div>
                 </div>
                 <div className="col-md-6 pt-4 pt-md-0 ps-md-4">
-                    <div className="card mb-4 border-0">
+                    <div className="card mb-4 border-0 bg-transparent">
                         <img src="./images/empresas.jpg" className="rounded d-md-none" alt="Empresas" />
                         <img src="./images/emprendedores.jpg" className="rounded d-none d-md-block" 
                             alt="Emprendedores" />
                         <div className="card-body px-0 pb-0 pt-md-4">
-                            <h4 className="fw-bolder display-8 mb-1">Para Empresas</h4>
+                            <h4 className="fw-bolder display-8 mb-1 text-center">Para Empresas</h4>
                             <p className="lead mb-3">Potencia tu equipo con soluciones personalizadas</p>
                             <p className="mb-3">
                                 Descubre servicios diseñados para impulsar la comunicación, liderazgo y 
@@ -49,6 +51,7 @@ export const Services = () => {
                                 organización crezca con éxito.
                             </p>
                             <p className="pb-4 fw-bold">¿Quieres conocer cómo potenciamos a las empresas?</p>
+                            <div className="text-center">
                             <Link to="/empresa">
                                 <button
                                     type="button"
@@ -56,6 +59,7 @@ export const Services = () => {
                                         <FontAwesomeIcon className="me-1" icon={faBuilding} /> Descubrir más
                                 </button>
                             </Link>    
+                            </div>
                         </div>
                     </div>
                 </div>
