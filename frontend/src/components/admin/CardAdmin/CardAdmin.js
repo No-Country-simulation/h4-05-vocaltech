52ff4f75cffdad8caeab60c651b00ca8282f1528
santgeek <santiago.mordz@gmail.com>
2025-01-26 21:05:52 -0500

    feat(template cards): implmented logic for dinamic template cards

--- frontend/src/components/admin/CardAdmin/CardAdmin.js
+++ frontend/src/components/admin/CardAdmin/CardAdmin.js
@@ -3,14 +3,14 @@ import { faEye } from "@fortawesome/free-solid-svg-icons";
 import { faPen } from "@fortawesome/free-solid-svg-icons";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
-export const CardAdmin = () => {
+export const CardAdmin = ({title, description}) => {
   return (
-    <div className="row">
-      <div className="card col-3" style={{ width: "18rem", height: "15rem" }}>
+    <>
+      <div className="card" style={{ width: "18rem", height: "15rem" }}>
         <div className="card-body">
-          <h5 className="card-title">Pitch</h5>
+          <h5 className="card-title">{title}</h5>
           <p className="card-text text-truncate">
-            Estimado usuario, en atención a la solicitud de tu pitch este es Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            {description}
           </p>
         </div>
         <div className="card-footer d-flex justify-content-center">
@@ -25,6 +25,6 @@ export const CardAdmin = () => {
           </button>
         </div>
       </div>
-    </div>
+    </>
   );
 };
