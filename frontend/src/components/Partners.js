9511e3826534978f2d6994deafe9a6c21020eb18
Maria Alarcon <mariagaa993@gmail.com>
2025-01-27 07:54:49 -0300

    cambiar texto en partners

--- frontend/src/components/Partners.js
+++ frontend/src/components/Partners.js
@@ -2,16 +2,24 @@ export const Partners = () => {
     return (
         <section className="container py-5">
             <h2 className="display-7 fw-bolder text-center pb-5">Nuestra alianza</h2>
-            <div className="row mb-4 align-items-center">
-                <div className="col-md-6 col-lg-5">
-                    <p className="pb-5 pb-md-0"><strong>Vos y Tu Voz</strong> y <strong>No Country</strong> se unen para 
-                        potenciar el talento desde distintos sectores y el desarrollo de aplicaciones
-                        innovadoras, ya seas emprendedor o líder de empresa.
+            <div className="row mb-4">
+                <div className="col-xl-10 text-xl-center mx-lg-auto">
+                    <p className="lead pb-3"><strong className="fw-bold">Vos y Tu Voz y No Country</strong> han formado una alianza estratégica 
+                        con un objetivo claro: transformar el talento humano y la innovación tecnológica en 
+                        los pilares del éxito empresarial.
+                    </p> 
+                    <p className="pb-3">Desde el acompañamiento a emprendedores en las primeras etapas 
+                        de sus proyectos hasta el fortalecimiento de equipos de liderazgo en grandes empresas, 
+                        <strong> nuestra colaboración ofrece soluciones personalizadas y de alto impacto</strong>. 
+                    </p>
+                    <p className="pb-5">Ya seas un emprendedor buscando inversores o una empresa
+                        interesada en desarrollar su equipo, nuestra alianza te brindará las herramientas 
+                        necesarias para <strong>alcanzar el éxito</strong>.
                     </p>
                 </div>
-                <div className="col-md-6 col-lg-7 text-center">
-                    <div className="row align-items-center">
-                        <div className="col-md-6 mb-5 mb-md-0">  
+                <div className="text-center">
+                    <div className="row align-items-center gap-5 gap-md-0">
+                        <div className="col-md-5 col-lg-4 col-xl-3 mx-auto me-lg-0">  
                             <img
                                 src="./images/logo-voz.png"
                                 className="img-fluid w-50"
@@ -19,10 +27,10 @@ export const Partners = () => {
                             
                             />
                         </div>
-                        <div className="col-md-6"> 
+                        <div className="col-md-6 col-lg-5 col-xl-4 mx-auto pe-md-5 ms-lg-0"> 
                             <img
                                 src="./images/logo-no-country.png"
-                                className="img-fluid "
+                                className="img-fluid"
                                 alt="No Country logo"
                             />
                         </div>
