8ff1ea7c4f5e8bb69b87a8276afe62bd7b54c455
Maria Alarcon <mariagaa993@gmail.com>
2025-01-29 12:39:51 -0300

    obtener datos al agendar una cita

---
+++ frontend/src/services/appointment.js
@@ -0,0 +1,52 @@
+import axios from "axios";
+
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+const TOKEN = process.env.REACT_APP_CALENDLY_API_TOKEN;
+
+const create = async () => {
+    try {
+        const response = await axios.post(`${BASE_URL}/appointments`);
+        return response.data;
+        
+    } catch  {
+        throw new Error("");
+    }
+};
+
+const eventDetails = async(url) => {
+    try {
+        const response = await axios.get(url,
+            {
+                headers: {
+                    Authorization: `Bearer ${TOKEN}`, 
+                }
+            }
+        );
+
+        return response.data;
+    } catch {
+        throw new Error("Error al obtener detalles del evento");
+    }
+};
+
+const inviteeDetails = async(url) => {
+    try {
+        const response = await axios.get(url,
+            {
+                headers: {
+                    Authorization: `Bearer ${TOKEN}`, 
+                }
+            }
+        );
+
+        return response.data;
+    } catch {
+        throw new Error("Error al obtener detalles del invitado");
+    }
+};
+
+export const appointmentService = {
+    create,
+    eventDetails,
+    inviteeDetails
+}; 
