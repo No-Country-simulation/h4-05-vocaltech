35fc2e0571ca0b9adea705af04312cec4beaeaad
Maria Alarcon <mariagaa993@gmail.com>
2025-01-28 23:33:02 -0300

    funcionalidad para traer los datos de la tabla lead. Filtrar según perfil -emprendedor/empresa-

--- frontend/src/views/admin/Leads.js
+++ frontend/src/views/admin/Leads.js
@@ -1,18 +1,41 @@
 import { useState, useEffect } from "react";
+import { Toaster, toast } from "sonner";
 import { columnsTable } from "../../utils/columnsTable";
 import { Table } from "../../components/admin/Table";
 import { SelectRole } from "../../components/SelectRole";
-import { prueba } from "../../utils/infoPrueba"; 
 import { useCompanySelect } from "../../contexts/CompanySelected";
+import { diagnosticService } from "../../services/diagnostic";
 
 export const Leads = () => {
     const [leadsData, setLeadsData] = useState([]);
     const { selectedCompany } = useCompanySelect();
     const [selectedRole, setSelectedRole] = useState("Todos");
+    const [filteredLeads, setFilteredLeads] = useState([]);
     
     useEffect(() => {
-        setLeadsData(prueba.leads);
+        const getData = async () => {
+            try {
+                const response = await diagnosticService.getLeads();
+                setLeadsData(response); 
+                setFilteredLeads(response);
+            } catch (error) {
+                toast.error(error.message);
+            } finally {
+
+            }
+        };
+
+        getData();
     }, []);
+
+    useEffect(() => {
+        if (selectedRole === 0 || selectedRole === "Todos") {
+            setFilteredLeads(leadsData); 
+        } else {
+            const filtered = leadsData.filter(lead => lead.profile.id === selectedRole);
+            setFilteredLeads(filtered); 
+        }
+    }, [selectedRole, leadsData]);
 
     return (
         <section>
@@ -23,15 +46,19 @@ export const Leads = () => {
                     <SelectRole all selectedRole={selectedRole} setSelectedRole={setSelectedRole} />
                 </div>
             </div>
-                {
-                    selectedCompany === 1 ? (
-                        'Leads Vos y tu voz'
-                    ) : selectedCompany === 2 ? (
-                        'Leads No Country'
-                    ) : (
-                        <Table columns={columnsTable.leads} data={leadsData} />
-                    )
-                }
+            {
+                selectedCompany === 1 ? (
+                    'Leads Vos y tu voz'
+                ) : selectedCompany === 2 ? (
+                    'Leads No Country'
+                ) : (
+                    <Table columns={columnsTable.leads} data={filteredLeads} />
+                )
+            }
+            <Toaster
+                richColors
+                position="top-center"
+            />
         </section>
     );
 };
