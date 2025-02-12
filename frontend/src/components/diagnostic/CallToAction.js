8db3ce884faf01c1c401c1bd6c27c7caaae6434f
santgeek <santiago.mordz@gmail.com>
2025-02-12 17:36:43 -0500

    buttons and content adjustments

--- frontend/src/components/diagnostic/CallToAction.js
+++ frontend/src/components/diagnostic/CallToAction.js
@@ -1,20 +1,16 @@
-import { Link } from "react-router-dom";
+
 
 export const CallToAction = () => {
     return (
         <section className="container py-5 mb-4 text-center">
             <div className="row align-items-center">
                 <div className="col-md-10 mx-auto">
-                    <h2 className="display-5 pb-4 fw-bolder">¿Prefieres agendar una cita en lugar de llenar el diagnóstico?
+                    <h2 className="display-5 pb-4 fw-bolder">¡No esperes más, realiza tu diagnótico ahora!
                     </h2>
-                    <p className="fw-bold pb-4">Reserva una sesión personalizada con nuestros expertos 
-                        para analizar tu situación en detalle. Estamos aquí para ayudarte. 
+                    <p className="fw-bold pb-4">Completa el proceso para que podamos asesorarte de la mejor manera. Recuerda que estamos aquí para brindarte el apoyo que necesitas. 
                     </p>
-                    <p className="lead pb-5">¡Elige la fecha y hora que mejor te convenga y démosle impulso a tu próximo paso!
+                    <p className="lead pb-5">¡Este es el momento, anímate a dar el siguiente paso en tu vida profesional!
                     </p>
-                    <Link to="/agendar-cita">
-                        <button className="btn btn-dark rounded-pill btn-personalized">Ir al calendario</button>
-                    </Link>
                 </div>
             </div>
         </section>
