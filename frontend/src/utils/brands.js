65c256d40be0e0dc5d99825c164df8ba655c954d
Maria Alarcon <mariagaa993@gmail.com>
2025-03-28 12:27:45 -0300

    agregar array de brands para iterar

---
+++ frontend/src/utils/brands.js
@@ -0,0 +1,6 @@
+export const brands = [
+    { id: 1, img: "/images/clients/brand1.png", width: "70" },
+    { id: 2, img: "/images/clients/brand2.png", width: "130" },
+    { id: 3, img: "/images/clients/brand3.png", width: "130" },
+    { id: 4, img: "/images/clients/brand4.png", width: "70" },
+];
