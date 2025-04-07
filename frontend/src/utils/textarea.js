7316d2390fdbfc308ec5a6b6bad295a371001b3b
Maria Alarcon <mariagaa993@gmail.com>
2025-04-07 11:47:47 -0300

    nuevo componente textarea para el formulario de contacto

---
+++ frontend/src/utils/textarea.js
@@ -0,0 +1,12 @@
+const contact = [
+    {
+        id: "message", 
+        label: "Mensaje", 
+        placeholder: "¿En qué te podemos ayudar?", 
+        type: "text" 
+    },
+];
+
+export const textarea = {
+    contact
+}
