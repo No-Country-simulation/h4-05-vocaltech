20d801fdac44a034e95b2550e4bb319c26be6fb5
Martín Kun <martinkunbrc@hotmail.com>
2025-04-16 11:01:55 -0300

    Update diagnostic.js

--- frontend/src/services/diagnostic.js
+++ frontend/src/services/diagnostic.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
-const API_GATEWAY_URL = process.env.API_GATEWAY_URL;
+const API_GATEWAY_URL = process.env.REACT_APP_API_GATEWAY_URL;
 
 const getLeads = async (token) => {
     try {
